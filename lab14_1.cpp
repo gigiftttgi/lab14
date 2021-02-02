87d548386a850dbd1c3cef19ab0d0c5eae625525
gigiftttgi <76035585+gigiftttgi@users.noreply.github.com>
2021-02-02 16:08:33 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,9 +1,23 @@
-include <iostream>
+#include<iostream>
 using namespace std;
 
-int main(){
-
-	
-	
-	return 0;
+int main()
+{
+	int a=5; 
+	int *x=&a;
+	int **z=&x;
+	char b='A';
+	char &c=b;
+	char *y;
+	y=&b;
+	cout << a << " " << b << " " << c << " " << x << " " << (void*)y << " " << z << "\n"; 
+	cout << &a << " " << (void*)&b << " " << (void*)&c << " " << &x << " " << &y << " " << &z << "\n"; 
+	c='F';
+	cout << a << " " << b << " " << c << " " << x << " " << (void*)y << " " << z << "\n"; 	 
+	*y='W';
+	cout << a << " " << b << " " << c << " " << x << " " << (void*)y << " " << z << "\n"; 
+	*x=6;
+	cout << a << " " << b << " " << c << " " << x << " " << (void*)y << " " << z << "\n"; 
+	**z=7;
+	cout << a << " " << b << " " << c << " " << x << " " << (void*)y << " " << z << "\n"; 	 	 	
 }
